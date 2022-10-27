e5e00da39654b4cfcc684bb6ba27a9eab2140bb5
tobiolusa <olusamiracle@gmail.com>
2022-10-27 03:45:08 +0100

    viewsupdate

--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from countries.models import Countries
 
 # Register your models here.
+
+admin.site.register(Countries)
+
--- countries/views.py
+++ countries/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
-# Create your views here.
+from countries.models import Countries
+from countries.serializers import CountriesSerializer
+from rest_framework.decorators import api_view
+
+@api_view(['GET', 'POST'])
+def countries_list(requests):
+    if requests.method == 'GET':
+        countries = countries.objects.all()
+        
+        
+        name = requests.GET.get('name', None)
+        if name is not None:
+            countries = countries.filter(name__icontans=name)
+            
+        countries_serializer = CountriesSerializer(countries, many=True)
+        return JsonResponse(countries_serializer.data, safe=False)
