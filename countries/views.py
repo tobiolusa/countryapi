f7ad2d819224642b5b65c204e003a183ca4ce6b2
tobiolusa <olusamiracle@gmail.com>
2022-10-29 22:07:15 +0100

    CountryModel Corrected

--- countries/views.py
+++ countries/views.py
@@ -13,14 +13,11 @@ from rest_framework.decorators import api_view
 
 @api_view(['GET', 'POST'])
 def countries_list(requests):
-    
     if requests.method == 'GET':
-        countries = countries.objects.all()
-        
-        
+        countries = Countries.objects.all()        
         name = requests.GET.get('name', None)
         if name is not None:
-            countries = countries.filter(name__icontans=name)
+            countries = Countries.filter(name__icontans=name)
             
         countries_serializer = CountriesSerializer(countries, many=True)
         return JsonResponse(countries_serializer.data, safe=False)
